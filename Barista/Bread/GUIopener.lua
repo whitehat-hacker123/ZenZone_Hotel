0496d5fee24b70820f1d4c7a2e9956df0be00450
Elliot <rising34sb@gmail.com>
2025-12-29 23:21:28 -0500

    Add functionality to open BreadGui
    Enable BreadGui for the player when the prompt is triggered.

--- Barista/Bread/GUIopener.lua
+++ Barista/Bread/GUIopener.lua
@@ -1,1 +1,10 @@
+local prompt = script.Parent
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+prompt.Triggered:Connect(function(player)
+	
+	local breadGui = player:WaitForChild("PlayerGui"):FindFirstChild("BreadGui")--add ur directory
+	if breadGui then
+		breadGui.Enabled = true
+	end
+end)
